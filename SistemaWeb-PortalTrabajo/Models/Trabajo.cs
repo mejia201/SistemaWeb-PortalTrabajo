c6383edbdd6dc1465bbe4a8928cbb9c3dff669cc
mejia201 <bryan.mejia@catolica.edu.sv>
2023-05-26 15:03:40 -0600

    Index terminado

--- SistemaWeb-PortalTrabajo/Models/Trabajo.cs
+++ SistemaWeb-PortalTrabajo/Models/Trabajo.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
 
 namespace SistemaWeb_PortalTrabajo.Models
@@ -10,17 +11,23 @@ namespace SistemaWeb_PortalTrabajo.Models
         public int idTrabajo  { get; set; }
         public string? titulo { get; set; }
         public string? descripcion { get; set; }
-        public string? requisitos { get; set; }
-        public string? ubicacion { get; set; }
-        public decimal salario { get; set; }
+        public string? requisitos { get; set; } 
+        public string? ubicacion { get; set; }  
+        public decimal salario { get; set; } 
 
-        public string? tipoContrato { get; set; }
+        public string? tipoContrato { get; set; }  
 
         public DateTime fechaPublicacion { get; set; }
         public DateTime? fechaVencimiento { get; set; }
 
+        [Column("idEmpresa")]
         public int idEmpresa { get; set; }
 
+        public string? estado { get; set; }
+
+
+        [ForeignKey("idEmpresa")]
+        public Empresa Empresa { get; set; }
 
 
     }
